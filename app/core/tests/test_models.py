ce028b35447e08a652d90f46f312b37b5ba21dc7
Frankie <frankie@mezanet.com>
2019-07-02 15:46:11 -0700

    Tags Api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@mezanet.com', password='test123'):
+    """Create sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -33,3 +41,12 @@ class ModelTest(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
